c5b44f96bdfe9aefed586c570a45c919722d0a19
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-09-27 14:39:37 -0700

    Add condition for query string in constructRequest

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ function fetchHar(har) {
 
 function constructRequest(har) {
   const requestObj = { method: har.method, headers: { [har.headers[0].name] : har.headers[0].value }, body: har.postData.text }
+  
+  if (har.queryString.length > 0) {
+    har.url = har.url + "?" + har.queryString[0].name + "=" + har.queryString[0].value;
+    return har.url
+  }
   return new Request(har.url, requestObj);
 }
 

e980b4a2cc511a5a2900d8a492c2e66d82e345be
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-09-28 15:11:49 -0700

    Configure eslint and npm scripts

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 function fetchHar(har) {
-  console.log(fetch(constructRequest(har)))
-  return fetch(constructRequest(har))
-    // .then((res) => {
-    //   return res
-    // })
-    // .then((json) => {
-    //   return (json)
-    // })
-    // .catch((err) => {
-    //   console.log(err)
-    // })
-};
+  console.log(fetch(constructRequest(har)));
+  return fetch(constructRequest(har));
+  // .then((res) => {
+  //   return res
+  // })
+  // .then((json) => {
+  //   return (json)
+  // })
+  // .catch((err) => {
+  //   console.log(err)
+  // })
+}
 
 function constructRequest(har) {
   let requestObj = {
     method: har.method,
-    body: har.postData.text
+    body: har.postData.text,
   };
 
   if (har.headers.length) {
-    requestObj.headers= {};
-    har.headers.forEach((header) => {
-      requestObj.headers[header.name] = header.value
-    })
+    requestObj.headers = {};
+    har.headers.forEach(header => {
+      requestObj.headers[header.name] = header.value;
+    });
   }
 
   if (har.queryString.length) {
-    har.url = har.url + "?"
+    har.url = har.url + '?';
     let str2;
     har.queryString.forEach((query, i) => {
-      str2 = har.queryString[i].name + "=" + har.queryString[i].value
-    })
-    har.url.concat(str2)
-    return har.url
+      str2 = har.queryString[i].name + '=' + har.queryString[i].value;
+    });
+    har.url.concat(str2);
+    return har.url;
   }
-  
+
   return new Request(har.url, requestObj);
 }
 

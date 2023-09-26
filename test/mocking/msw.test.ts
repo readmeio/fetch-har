8f7d0c44b5768df75aca556918eddb9e5de24c34
Kanad Gupta <git@kanad.dev>
2023-09-26 18:53:33 -0500

    test: add MSW tests

---
+++ test/mocking/msw.test.ts
@@ -0,0 +1,23 @@
+import harExamples from 'har-examples';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { describe, it, expect } from 'vitest';
+
+import fetchHAR from '../../src';
+
+describe('#fetchHAR mocking (msw)', () => {
+  it('should support mocking a request with `msw` (server)', async () => {
+    const server = setupServer(
+      rest.get('https://httpbin.org/get', (req, res, ctx) => {
+        return res(ctx.status(429));
+      }),
+    );
+
+    server.listen();
+
+    const res = await fetchHAR(harExamples.short);
+    expect(res.status).toBe(429);
+
+    server.close();
+  });
+});

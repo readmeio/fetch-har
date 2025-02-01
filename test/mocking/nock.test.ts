22678417f1f961766699056fd1bfbfd85313cbb2
Jon Ursenbach <jon@ursenba.ch>
2025-01-31 23:17:46 -0800

    feat: adding a unit test for `nock` mocking

---
+++ test/mocking/nock.test.ts
@@ -0,0 +1,16 @@
+import harExamples from 'har-examples';
+import nock from 'nock';
+import { describe, it, expect } from 'vitest';
+
+import fetchHAR from '../../src/index.js';
+
+describe('#fetchHAR mocking (nock)', () => {
+  it('should support mocking a request with `nock`', async () => {
+    nock('https://httpbin.org').get('/get').reply(429);
+
+    const res = await fetchHAR(harExamples.short);
+    expect(res.status).toBe(429);
+
+    nock.restore();
+  });
+});

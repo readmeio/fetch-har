6622d99a1eb8567fd077737dfdf49800eee2d596
Jon Ursenbach <jon@ursenba.ch>
2025-01-31 23:16:01 -0800

    chore: upgrading `fetch-mock`

--- test/mocking/fetch-mock.test.ts
+++ test/mocking/fetch-mock.test.ts
@@ -6,17 +6,16 @@ import fetchHAR from '../../src/index.js';
 
 describe('#fetchHAR mocking (fetch-mock)', () => {
   it('should support mocking a request with `fetch-mock`', async () => {
-    fetchMock.mock(
-      {
-        url: 'https://httpbin.org/get',
-        method: 'get',
-      },
-      new Response(null, { status: 429 }),
-    );
+    fetchMock.mockGlobal().route({
+      url: 'https://httpbin.org/get',
+      method: 'get',
+      response: new Response(null, { status: 429 }),
+    });
 
     const res = await fetchHAR(harExamples.short);
     expect(res.status).toBe(429);
 
-    fetchMock.restore();
+    fetchMock.clearHistory();
+    fetchMock.unmockGlobal();
   });
 });
